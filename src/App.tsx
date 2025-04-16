4d0ed95191590338bed39c1410582f469b9487e0
tan.nguyenvan <tan.nguyenvan@ncc.asia>
2025-04-16 10:09:06 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Testimonial from './components/Testimonial'
+import CallToAction from './components/CallToAction'
+import Footer from './components/Footer'
+import Features from './components/Features'
+function App() {
+
+  return (
+    <>
+        <div className="text-blue-400">Test màu</div>
+      {/* <BrowserRouter>
+        <Navbar />
+        <Hero />
+        <Features />
+        <Testimonial />
+        <CallToAction />
+        <Footer />
+      </BrowserRouter> */}
+    </>
+  )
+}
+
+export default App
